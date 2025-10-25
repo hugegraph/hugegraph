921ae9c7253cbea4c995f5fc47140d9086f47549
imbajin <jin@apache.org>
2025-10-25 21:57:57 +0800

    Update hugegraph-store/hg-store-core/src/main/java/org/apache/hugegraph/store/meta/GraphIdManager.java

--- hugegraph-store/hg-store-core/src/main/java/org/apache/hugegraph/store/meta/GraphIdManager.java
+++ hugegraph-store/hg-store-core/src/main/java/org/apache/hugegraph/store/meta/GraphIdManager.java
@@ -41,6 +41,7 @@ import lombok.extern.slf4j.Slf4j;
 public class GraphIdManager extends PartitionMetaStore {
 
     protected static final String GRAPH_ID_PREFIX = "@GRAPH_ID@";
+    // FIXME: we need to ensure the right num & proper logic for it (IMPORTANT)
     protected static int maxGraphID = 65535 - 1;
     static Object graphIdLock = new Object();
     static Object cidLock = new Object();
